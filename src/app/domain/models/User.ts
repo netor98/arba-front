61c381d08250ebcd858160384a8a67ca592a1695
napo <pcu18021379@gmail.com>
2024-11-03 23:49:09 -0700

    Skeleton clean arch

---
+++ src/app/domain/models/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+  role: string;
+}
